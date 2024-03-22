f70a703d8ab026b58c34458c0025e7bc493c4b71
lokeshdhote <lokeshdhote2004@gmail.com>
2024-03-23 00:53:29 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+const userModel  = require("./users")
+
+// const productModel  = require("./product")
+// const upload = require("./multer")
+const passport = require("passport")
+const localStrategy = require("passport-local");
+
+passport.use(new localStrategy (userModel.authenticate()))
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', );
+});
+
+
+router.post("/register",function(req,res,next ){
+  var userdata = new userModel({
+    username :req.body.username,
+    email:req.body.email,
+    category:req.body.category,
+    secret:req.body.secret
+  })
+  
+  userModel.register(userdata,req.body.password)
+  .then(function(registereduser){
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/home")
+  
+    })
+  })
+})
+
+ 
+router.post("/login" , passport.authenticate("local",{
+  successRedirect:"/home",
+  failureRedirect:"/"
+ 
+
+}),function(req,res){ })
+
+router.get("/logout",function(req,res,next){
+  req.logout(function(err){
+    if(err) return next(err)
+    res.redirect("/")
+  })
+})
+
+
+function isLoggedIn(req,res,next){
+if(req.isAuthenticated ()){
+  return next()
+}
+res.redirect("/")
+}
+
+module.exports = router;
